fb4cf7217fd2eaaeca75afabe6b9dafb173e52cd
Suning Yao <sy2940@nyu.edu>
2021-10-28 17:48:36 -0400

    Update 01-stringCompare.js

--- 01-stringCompare.js
+++ 01-stringCompare.js
@@ -1,7 +1,14 @@
 const string0 = 'woohoo';
 const string1 = 'yeah';
+const string2 = 'yeah';
 
 if (string0 === string1) {
+  alert('same strings');
+} else {
+  alert('!same strings');
+}
+
+if (string2 === string1) {
   alert('same strings');
 } else {
   alert('!same strings');
